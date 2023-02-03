7dd6f344a411587b835ceb21ef3f7785a94f458c
Joshua Kim <joshua0702@gmail.com>
2023-02-03 14:42:03 -0800

    testing: test interpolation within board

--- test/lib/VolGeneratorWithinBoard.t.sol
+++ test/lib/VolGeneratorWithinBoard.t.sol
@@ -12,9 +12,8 @@ contract VolGeneratorTest is Test {
     tester = new VolGeneratorTester();
   }
 
-  function testStrikeInterpolation() public {
+  function testInterpolation() public {
     // initial setup
-    uint spot = 1496.2e18;
     uint[] memory strikes = new uint[](5);
     strikes[0] = 1400e18;
     strikes[1] = 1450e18;
@@ -39,6 +38,47 @@ contract VolGeneratorTest is Test {
     // Strike: $1425
     uint newSkew = tester.getSkewForLiveBoard(1425e18, liveBoard);
     assertApproxEqAbs(newSkew, 1.1515245649507764e18, 1e10);
+
+    // Strike: $1450
+    newSkew = tester.getSkewForLiveBoard(1450e18, liveBoard);
+    assertApproxEqAbs(newSkew, 1.2e18, 1e10);
+
+    // Strike: $1569
+    newSkew = tester.getSkewForLiveBoard(1569e18, liveBoard);
+    assertApproxEqAbs(newSkew, 1.0350546610797016e18, 1e10);
+  }
+
+  function testInterpolationNearExistingStrikes() public {
+    // initial setup
+    uint[] memory strikes = new uint[](5);
+    strikes[0] = 1300e18;
+    strikes[1] = 1400e18;
+    strikes[2] = 1500e18;
+    strikes[3] = 1600e18;
+    strikes[4] = 1700e18;
+
+    uint[] memory skews = new uint[](5);
+    skews[0] = 1.32e18;
+    skews[1] = 1.1e18;
+    skews[2] = 0.92e18;
+    skews[3] = 0.6e18;
+    skews[4] = 1.2e18;
+
+    VolGenerator.Board memory liveBoard = VolGenerator.Board({
+      tAnnualized: _secToAnnualized(21 days),
+      baseIv: 0.66e18,
+      orderedStrikePrices: strikes,
+      orderedSkews: skews
+    });
+
+    // Strike: $1300.00001
+    uint newSkew = tester.getSkewForLiveBoard(1300.00001e18, liveBoard);
+    assertApproxEqAbs(newSkew, 1.3199999790672716e18, 1e10);
+
+    // Strike: $1599.99
+    newSkew = tester.getSkewForLiveBoard(1599.99e18, liveBoard);
+    assertApproxEqAbs(newSkew, 0.6000392518815546e18, 1e10);
+
   }
 
   /////////////
