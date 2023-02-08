beeacba62d17b1d022691c2edd31bdf65b3fb5db
Beefmaster <cameron.j.baker1000@gmail.com>
2023-02-09 10:59:17 +1100

    more work on testing

--- test/lib/ExpiryGeneratorTest.t.sol
+++ test/lib/ExpiryGeneratorTest.t.sol
@@ -1,6 +1,20 @@
+//SPDX-License-Identifier: ISC
+pragma solidity 0.8.16;
 
+import "openzeppelin/utils/Arrays.sol";
+import "";
 import "forge-std/test";
 
 contract ExpiryGeneratorTest is Test {
+  using stdJson for string;
+
+  uint[] fridays;
+
+  function setUp() public {
+    // load fridays.json into strikePriceTester
+    string memory path = string.concat(vm.projectRoot(), "/script/params/fridays.json");
+    string memory json = vm.readFile(path);
+    fridays = json.readUintArray(".fridays");
+  }
 
 }
